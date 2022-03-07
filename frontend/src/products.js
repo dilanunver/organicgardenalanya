6166ed262a99d4491675b840154184680e7ad7e7
dilanunver <dunver33@gmail.com>
2022-03-07 10:33:49 +0100

    backend & fronend

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    id: '1',
+    name: 'Avocado',
+    image: '/organikbahcemalanyaimages/avocadotree.jpg',
+    description:
+      "Dalindan taze toplayip size göndereceğimiz avokado ile en lezzetli salatalari yapabilir ve kahvaltilariniza ekleyip güne enerji dolu başlabilirsiniz. Siparişinize eklemeyi unutmayiniz.",
+    brand: 'Organik Bahcem Alanya',
+    category: 'fruits',
+    price: 8.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    id: '2',
+    name: 'Banana',
+    image: '/organikbahcemalanyaimages/banana.jpg',
+    description:
+      'Muz doyurucu ve besleyici özelliği ile evlerimiz vazgeçilmez meyvesi olmuştur. Hazirlayacağiniz pasta,tatli ve içeceklerinizde muz ile muhteşem lezzetler yaratabiliriz. Çocuklarimiza güvenle yedireceğimiz yerli muzlar için sipariş vermeyi ihmal etmeyin!',
+    brand: 'Organik Bahcem Alanya',
+    category: 'fruits',
+    price: 5.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    id: '3',
+    name: 'Lemon',
+    image: '/organikbahcemalanyaimages/lemon.jpg',
+    description:
+      'Asirlik bahçemizden sizler için olgun ve en sulu limonlari seçip kapiniza kadar gönderiyoruz. Tam bir C vitamini deposu olan limonu mutfağinizdan eksik etmeyin.',
+    brand: 'Organik Bahcem Alanya',
+    category: 'fruits',
+    price: 9.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    id: '4',
+    name: 'Mango',
+    image: '/organikbahcemalanyaimages/mango.jpg',
+    description:
+      'Hindistan’dan dünyaya yayilmiş olup Orta ve Güney Amerika, Asya ve Afrika yetişmektedir.A, B, C, Fosfor, Antioksidan, Beta Karoten, Petkin İçermektedir',
+    brand: 'Organik Bahcem Alanya',
+    category: 'fruits',
+    price: 39.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    id: '5',
+    name: 'Orange',
+    image: '/organikbahcemalanyaimages/orange.jpg',
+    description:
+      'C vitamini açisindan zengin olan portakal suyunu bir de bizden alacağiniz portakallar ile deneyin. Portakallarimizi sipariş verdiğiniz an dalindan toplayip hemen sizlere gönderdiğimiz için portakallarimizda herhangi bir koruyucu madde kullanmamaktayiz.',
+    brand: 'Organik Bahcem Alanya',
+    category: 'fruits',
+    price: 4.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    id: '6',
+    name: 'Papaya',
+    image: '/organikbahcemalanyaimages/papaya.jpg',
+    description:
+      'Tropik bir meyve olan papayayi sizler için yetiştirdik. Ve sizlerle paylaşmak en büyük mutluluğumuz olacaktir. Eğer denemediyseniz mutlaka sepetinize eklemelisiniz.',
+    brand: 'Organik Bahcem Alanya',
+    category: 'fruits',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
