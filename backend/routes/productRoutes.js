3c18c4e85eb75c0ad0aa5e2cf1fb8b990a4818c1
dilanunver <dunver33@gmail.com>
2022-03-11 14:07:08 +0100

    custom error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -20,14 +20,13 @@ router.get('/', asyncHandler(async (req, res) => {
 // @access Public
 
 router.get('/:id', asyncHandler(async (req, res) => {
-  console.log(req)
   const product = await Product.findById(req.params.id)
-  console.log(req.params.id)
-  if (product) {
 
+  if (product) {
     res.json(product)
   } else {
-    res.status(404).json({ message: 'Product not found' })
+    res.status(404)
+    throw new Error('Product not found')
   }
 }))
 
--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,20 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
-
 import productRoutes from '../backend/routes/productRoutes.js'
+
+import { errorHandler, notFound } from '../backend/middleware/errorMiddleware.js'
 
 dotenv.config()
 
 connectDB()
 const app = express()
+
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.get('/', (req, res) => {
   res.send('Api is running...5')
